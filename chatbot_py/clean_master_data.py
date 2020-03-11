30fb8564aac375b630005d3439b22786c069787b
Emily Medema <medema.emily@gmail.com>
2020-03-11 16:15:49 -0700

    adding test file

--- chatbot_py/clean_master_data.py
+++ chatbot_py/clean_master_data.py
@@ -7,26 +7,26 @@ def clean_data(data):
     cleaned_data = DataFrame(columns=["Question", "Answer"])
     # For all lines in the data dataframe
     for j in data.iterrows():
-        # Appends the cleaned question and response to the cleaned_data array 
+        # Appends the cleaned question and response to the cleaned_data array
         cleaned_data = cleaned_data.append(
             {
-                "Question":tokenize_and_tag_line(clean_line(j[1][0])), 
+                "Question":tokenize_and_tag_line(clean_line(j[1][0])),
                 "Answer":tokenize_and_tag_line(clean_line(j[1][1]))
-            }, 
+            },
             ignore_index=True)
     return cleaned_data
-    
+
 
 def clean_line(line):
-    # This line makes all lowercase, and removes anything that isn't a number 
+    # This line makes all lowercase, and removes anything that isn't a number
     return sub(r'[^a-z ]', '', str(line).lower())
 
 
 def tokenize_and_tag_line(line):
-    # Tokenizes the words then tags the tokenized words 
+    # Tokenizes the words then tags the tokenized words
     return pos_tag(word_tokenize(line), None)
 
 
-df = read_csv("data/master_data.csv")
+df = read_csv("../data/master_data.csv")
 cleaned_data = clean_data(df)
-cleaned_data.to_pickle("data/master_data_cleaned.pkl")
+cleaned_data.to_pickle("../data/master_data_cleaned.pkl")
---
+++ chatbot_py/test.py
@@ -0,0 +1,9 @@
+import unittest
+import laur.ai
+import combine_data_to_master
+import clean_master_data
+import processtranscript
+
+
+
+
